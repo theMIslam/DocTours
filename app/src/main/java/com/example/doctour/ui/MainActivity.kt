f71493fe630efddfb34b6665f10c8319072a2496
DELL <aycholpon25t@gmail.com>
2023-05-15 12:35:10 +0600

    разделение по клину

--- app/src/main/java/com/example/doctour/ui/MainActivity.kt
+++ app/src/main/java/com/example/doctour/ui/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.doctour.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.doctour.R
+import com.example.doctour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
