455158d99101f284f76db13aaace4f676c5867f9
theMIslam <valvekakaxa@gmail.com>
2023-05-18 12:51:43 +0600

    fix anim splashscreen

---
+++ app/src/main/java/com/example/doctour/ui/fragments/splash/SplashScreenViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.doctour.ui.fragments.splash
+
+import com.example.doctour.core.base.BaseViewModel
+import javax.inject.Inject
+
+class SplashScreenViewModel @Inject constructor(
+
+) : BaseViewModel() {
+
+}
