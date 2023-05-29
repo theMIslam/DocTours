e5ac6f34e9414d9e7d64542ca997d65e7bea0de4
theMIslam <valvekakaxa@gmail.com>
2023-05-29 10:53:02 +0600

    error's app module (потом фиксить буду)

--- app/src/main/java/com/example/doctour/data/remote/dto/SignInResultDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.doctour.data.remote.dto
-
-data class SignInResultDto(
-    val id: String,
-    val tokens: String,
-    val username: String
-)
--- app/src/main/java/com/example/doctour/data/remote/dto/SignInWithGoogleDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.doctour.data.remote.dto
-
-import com.google.gson.annotations.SerializedName
-
-data class SignInWithGoogleDto(
-    @SerializedName("access_token")
-    val idToken: String
-)
