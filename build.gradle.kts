280beb44ca2f7f99aa601d97435ae8809ea80be7
theMIslam <valvekakaxa@gmail.com>
2023-06-02 14:19:58 +0600

    buildSrc, kotlin-dsl

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id(Plugins.AGP.application) version Versions.AGP apply false
+    id(Plugins.AGP.library) version Versions.AGP apply false
+    id(Plugins.Kotlin.android) version Versions.kotlin apply false
+    id(Plugins.DaggerHilt.hilt) version Versions.hilt apply false
+    id(Plugins.Kotlin.jvm) version Versions.kotlin apply false
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Doctour"
-include ':app'
+include (":app")
