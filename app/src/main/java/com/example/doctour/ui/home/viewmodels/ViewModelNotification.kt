993b11cf036635d88d8284c6eae870cb44486fdf
Tashpolotov <Aza662485@gmail.com>
2023-06-03 20:45:19 +0600

    сделал переход с categoryFragment  и notificationFragment

---
+++ app/src/main/java/com/example/doctour/ui/home/viewmodels/ViewModelNotification.kt
@@ -0,0 +1,23 @@
+package com.example.doctour.ui.home.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.doctour.R
+import com.example.doctour.core.base.BaseViewModel
+import com.example.doctour.ui.home.Notification
+
+class ViewModelNotification :BaseViewModel() {
+
+    private val _notification = MutableLiveData<List<Notification>>()
+    val notification: LiveData<List<Notification>> get() = _notification
+
+    fun getNotification(){
+        _notification.value = listOf(Notification("Запись успешна",
+            "вы успешно записались на встречу", R.drawable.ic_successfully.toString(), "15:00"),
+            Notification("Запись отменена!",
+                "Вы успешно отменили встречу", R.drawable.ic_failed.toString(), "10:00"),
+            Notification("Запись измена!",
+                "Вы успешно изменили встречу", R.drawable.ic_change.toString(), "16:00"),
+        )
+    }
+}
