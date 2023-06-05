9e87d3a1a4769deb23a5eae012d445ad6776852b
aycholpon-bektenbekova <aicholpon25t@gmail.com>
2023-06-05 14:28:18 +0600

    login package

--- app/src/main/java/com/example/doctour/presentation/ui/password/SuccessPasswordFragment.kt
+++ app/src/main/java/com/example/doctour/presentation/ui/password/SuccessPasswordFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.doctour.R
+import androidx.navigation.fragment.findNavController
+import com.example.doctour.databinding.FragmentSuccessPasswordBinding
 
 class SuccessPasswordFragment : Fragment() {
+
+    private lateinit var binding : FragmentSuccessPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_success_password, container, false)
+    ): View {
+        binding = FragmentSuccessPasswordBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickers()
+    }
+
+    private fun clickers() {
+        binding.btnDone.setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
 }
