28d39598868d134bde186701a6e0a631b5d9a39a
theMIslam <valvekakaxa@gmail.com>
2023-06-08 16:16:18 +0600

    fix conflicta

--- app/src/main/java/com/example/doctour/presentation/ui/fragments/appoiment/CancelledBAdapter.kt
+++ app/src/main/java/com/example/doctour/presentation/ui/fragments/appoiment/CancelledBAdapter.kt
@@ -3,7 +3,7 @@ package com.example.doctour.presentation.ui.fragments.appoiment
 import androidx.recyclerview.widget.DiffUtil
 import com.example.doctour.presentation.ui.fragments.appoiment.model.BermetModel
 
-private class CancelledBDifUtil:DiffUtil.ItemCallback<BermetModel>(){
+class CancelledBDifUtil:DiffUtil.ItemCallback<BermetModel>(){
     override fun areItemsTheSame(oldItem: BermetModel, newItem: BermetModel): Boolean {
         return oldItem.id==newItem.id
     }
--- app/src/main/java/com/example/doctour/presentation/ui/fragments/appoiment/UpComingBAdapter.kt
+++ app/src/main/java/com/example/doctour/presentation/ui/fragments/appoiment/UpComingBAdapter.kt
@@ -4,7 +4,7 @@ package com.example.doctour.presentation.ui.fragments.appoiment
 import androidx.recyclerview.widget.DiffUtil
 import com.example.doctour.presentation.ui.fragments.appoiment.model.BermetModel
 
-private class UpComingDifUtil: DiffUtil.ItemCallback<BermetModel>() {
+class UpComingDifUtil: DiffUtil.ItemCallback<BermetModel>() {
     override fun areItemsTheSame(oldItem: BermetModel, newItem: BermetModel): Boolean {
         return oldItem.id==newItem.id
     }
