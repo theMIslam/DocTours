2d342ac9e8f9aedcd2332a969ff0e66298902451
Dilnaz27 <dbaudunova@gmail.com>
2023-06-11 12:46:55 +0600

    changed design everywhere except login/sign up screen

---
+++ app/src/main/java/com/example/doctour/presentation/ui/fragments/password/ForgotPasswordFragment.kt
@@ -0,0 +1,16 @@
+package com.example.doctour.presentation.ui.fragments.password
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.doctour.R
+import com.example.doctour.databinding.FragmentForgotPasswordBinding
+import com.example.doctour.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ForgotPasswordFragment() : BaseFragment<FragmentForgotPasswordBinding, PasswordViewModel>(
+    R.layout.fragment_forgot_password
+) {
+    override val binding: FragmentForgotPasswordBinding by viewBinding(FragmentForgotPasswordBinding::bind)
+    override val viewModel: PasswordViewModel by viewModels<PasswordViewModel>()
+}
