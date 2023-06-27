b0c41a5b218db5fe65566863e62e3a7965141da3
aycholpon-bektenbekova <aicholpon25t@gmail.com>
2023-06-27 18:41:54 +0600

    register api

--- data/src/main/java/com/example/doctour/data/viewmodels/UserRegisterVIewModel.kt
+++ data/src/main/java/com/example/doctour/data/viewmodels/UserRegisterVIewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.doctour.data.local.db.entities.doctour.UserRegisterEntity
 import com.example.doctour.data.remote.apiservices.UserRegisterApiService
+import com.example.doctour.data.remote.client.RetrofitClient
 
 class UserRegisterVIewModel: ViewModel(){
+
+    private val apiService: UserRegisterApiService by lazy {
+        RetrofitClient.create()
+    }
 
     fun usersRegister(): LiveData<UserRegisterEntity> {
         return postUserRegister()
@@ -13,6 +18,5 @@ class UserRegisterVIewModel: ViewModel(){
 
     private fun postUserRegister(): LiveData<UserRegisterEntity> {
 
-        //UserRegisterApiService.usersRegister
     }
 }
