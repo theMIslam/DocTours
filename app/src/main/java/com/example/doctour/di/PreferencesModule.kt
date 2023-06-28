2453a898394e9276de1cdf2cd6110504ac1fad7b
theMIslam <valvekakaxa@gmail.com>
2023-06-29 00:16:44 +0600

    dto

--- app/src/main/java/com/example/doctour/di/PreferencesModule.kt
+++ app/src/main/java/com/example/doctour/di/PreferencesModule.kt
@@ -3,6 +3,7 @@ package com.example.doctour.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.example.doctour.data.local.preferences.PreferencesHelper
+import com.example.doctour.di.PreferencesKeys.DOCTOUR_SHARED_PREFERENCES
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,4 +27,11 @@ object PreferencesModule {
         @ApplicationContext context: Context
     ) = UserPreferences(context)
 
+    @Singleton
+    @Provides
+    fun generateSharedPreferences(@ApplicationContext context: Context): SharedPreferences =
+        context.getSharedPreferences(
+            DOCTOUR_SHARED_PREFERENCES,
+            Context.MODE_PRIVATE
+        )
 }
