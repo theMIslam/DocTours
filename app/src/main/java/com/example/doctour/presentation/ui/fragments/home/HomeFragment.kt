c99d9f063e218d2a87382904ba5830064b20ab15
aycholpon-bektenbekova <aicholpon25t@gmail.com>
2023-06-29 03:14:08 +0600

    block code

--- app/src/main/java/com/example/doctour/presentation/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/doctour/presentation/ui/fragments/home/HomeFragment.kt
@@ -14,8 +14,11 @@ import com.example.doctour.presentation.ui.fragments.home.adapter.AdapterHomeDoc
 import com.example.doctour.presentation.ui.fragments.home.adapter.AdapterHomeInfoDoctor
 import com.example.doctour.presentation.ui.fragments.home.model.DoctorModel
 import com.example.doctour.presentation.ui.fragments.home.model.HomeModel
+import com.example.doctour.presentation.ui.fragments.main.category.observer.TextUpdate
 import dagger.hilt.android.AndroidEntryPoint
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 @AndroidEntryPoint
 class HomeFragment() : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home),
@@ -121,6 +124,10 @@ class HomeFragment() : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout
         }
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onTextUpdate(event: TextUpdate) {
+        binding.tvCountry.text = event.newText
+    }
     override fun onStart() {
         super.onStart()
         EventBus.getDefault().register(this)
